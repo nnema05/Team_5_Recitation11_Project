05e2dc2fbceacd0dbf3347757288c3c59a2524bb
Soumya <soumyadevul@gmail.com>
2024-11-07 11:10:45 -0700

    new redirect test case

--- ProjectSourceCode/test/servr.spec.js
+++ ProjectSourceCode/test/servr.spec.js
@@ -97,6 +97,9 @@ describe('Testing register API', () => {
 });
 
 
+
+
+
 // describe('Testing Redirect', () => {
 //   it('should redirect / route to /login with 302 HTTP status code', done => {
 //     chai
@@ -194,3 +197,19 @@ after(() => {
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
 // ********************************************************************************
+
+
+// REDIRECT TEST CASE 
+// describe('Testing Redirect', () => {
+//   // Sample test case given to test /test endpoint.
+//   it('\test route should redirect to /login with 302 HTTP status code', done => {
+//     chai
+//       .request(server)
+//       .get('/login')
+//       .end((err, res) => {
+//         res.should.have.status(302); // Expecting a redirect status code
+//         res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
+//         done();
+//       });
+//   });
+// });
