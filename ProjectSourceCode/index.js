7c083e20022932d5106060546205846c4693dbc9
Mary Kodenkandath <mako6689@colorado.edu>
2024-11-07 10:19:47 -0700

    updates index.js

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -79,6 +79,10 @@ app.use(
 
 
 // TODO - Include your API routes here
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
+
 app.get('/', (req, res) => {
   res.redirect('/login'); // Redirect to the /login route
 });
@@ -177,5 +181,5 @@ app.get('/logout', (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
