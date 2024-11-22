9aea35d30f135e6677aa3e06f3ba8048730e0760
Nandini Nema <nandininema05@gmail.com>
2024-11-22 09:20:39 -0700

    DEPLOYMENT SO WOKRS YAYY

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -7,7 +7,7 @@
 PG_URI="postgresql://users_db_73jj_user:DtpyMQ09qOFmAx2pZAbGoxGaPYIrFQO1@dpg-csvmdtt2ng1s73dtk83g-a.oregon-postgres.render.com/users_db_73jj"
 
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in src/init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
